7ecb2329fbc2f1392c31c2dc999d453814b49258
Andres Uribe <andresuribe@andress-air.lan>
2025-08-14 15:16:59 -0700

    Move requirements.txt and remove unused dependencies in app.py

--- src/app.py
+++ src/app.py
@@ -1,7 +1,5 @@
 # app.py
 import streamlit as st
-import pandas as pd
-import matplotlib.pyplot as plt
 from streamlit_option_menu import option_menu
 from components.login import show_login
 from components.predict import show_predict
